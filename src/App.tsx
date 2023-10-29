9516f92c17ff1752ae33388380699335e97b13bd
parker cheng <fbi0258zzz@gmail.com>
2023-10-29 16:28:30 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <header></header>
+      <main>
+        <h1>Hello</h1>
+        <div className=""></div>
+      </main>
+      <footer></footer>
+    </>
+  );
+}
+
+export default App;
